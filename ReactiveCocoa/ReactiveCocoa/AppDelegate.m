c8a3fdc37b8489f53a6be9c8a4dc4a9b080a7cb8
koala <wiki@deerbike.cn>
2018-06-14 10:23:52 +0800

    1

--- ReactiveCocoa/ReactiveCocoa/AppDelegate.m
+++ ReactiveCocoa/ReactiveCocoa/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"123");
     return YES;
 }
 
